26e21bac4bdef4cfbcfeb233df872399bffc62a1
robinraju <robinraju@powa.com>
2016-02-24 18:06:07 +0530

    Modified Database connection properties

--- src/main/java/gq/smallurl/utils/DbHelper.java
+++ src/main/java/gq/smallurl/utils/DbHelper.java
@@ -11,34 +11,21 @@ import java.sql.SQLException;
  * Helper class to connect to MySQL Database
  * */
 public class DbHelper {
-	private static Connection connection;
-	static String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-	static String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-	private static String url = "jdbc:mysql://" + host + ":" + port + "/smallurl";
+	private static Connection connection;	
 	private static String driver = "com.mysql.jdbc.Driver";
 
     /**
      * Connects to MySQL DB
      * @return connection
      * */
-	public static Connection connectToDb() {
-		try {
-			Class.forName(driver);
-			connection = DriverManager.getConnection(url, "admin92ydYPj", "knqRuGBj8_9D");
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}
-	
-	/*public static Connection connectToDb() {
-		try {
-			Class.forName(driver);
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smallurl", "root", "");
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}*/
+    public static Connection connectToDb() {
+    	try {
+    		Class.forName(driver);
+    		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smallurl", "root", "");
+    	} catch (ClassNotFoundException | SQLException e) {
+    		e.printStackTrace();
+    	}
+    	return connection;
+    }
 
 }
